bb78f7410c6c679c28e08492e511b9ee65d400d4
NaijaTechBro <bakisodiq@gmail.com>
2026-04-24 10:41:55 +0100

    helo

--- models/Template.js
+++ models/Template.js
@@ -1,16 +1,6 @@
 
 
-// const mongoose = require('mongoose');
 
-// const templateSchema = new mongoose.Schema({
-//     testCode: { type: String, required: true, unique: true }, // e.g., 'WIDAL', 'MP'
-//     testName: { type: String, required: true }, // e.g., 'Widal Reaction'
-//     category: { type: String, required: true }, // e.g., 'Serology'
-    
-//     // The JSON configuration that drives the dynamic React UI
-//     schemaDefinition: [
-//         {
-//             fieldName: { type: String, required: true }, 
 //             label: { type: String, required: true }, 
 //             inputType: { 
 //                 type: String, 
